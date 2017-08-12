4c701a7df5f8cebcb3b9a34469649e3edbcb50c7
Catter38 <daniel@cap-web.at>
2017-08-12 14:50:56 +0200

    Added the first API Function (which is just a test btw)

---
+++ Controllers/API/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tudseon.Controllers.API
+{
+    [Route("API/[controller]")]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Test", "GAAAAAH" };
+        }
+    }
+}
